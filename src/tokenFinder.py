88cbfa6a2ef9d348cb32ae6a3447a77e77c7d8e6
TanujKS <tanujsiripurapu@gmail.com>
2021-06-21 12:41:46 -0700

    Stable tokenFinder

--- src/tokenFinder.py
+++ src/tokenFinder.py
@@ -1,46 +1,86 @@
 import os
 import re
 
-
-def findTokens():
-    tokenList = []
-    lib = f"/Users/{os.getlogin()}/Library/Application Support/"
-
-    paths = {
-    'Discord': lib + 'discord/Local Storage/leveldb',
-    }
-    for i in range(1, 100):
-        path = lib + f"/Google/Chrome/Profile {i}/Local Storage/leveldb"
-        if not os.path.exists(path):
-            continue
-        paths[f'Google Chrome Profile {i}'] = path
-
-    for platform, path in paths.items():
-        if not os.path.exists(path):
-            continue
-        for token in extract_tokens(path):
-            tokenList.append(token)
-
-    return tokenList
+from sys import platform
+if platform != "darwin":
+    input("WARNING: This program is only supported for macOS \nThere is no gaurantee on the functionality of this program on this device. \nPress enter to continue")
 
 
-def extract_tokens(path):
-    tokens = []
-    for file_name in os.listdir(path):
-        if not file_name.endswith('.log') and not file_name.endswith('.ldb'):
-            continue
-        for line in [x.strip() for x in open(f'{path}/{file_name}', errors='ignore').readlines() if x.strip()]:
-            for regex in (r'[\w-]{24}\.[\w-]{6}\.[\w-]{27}', r'mfa\.[\w-]{84}'):
-                for token in re.findall(regex, line):
-                    tokens.append(token)
-    return tokens
+class Token:
+    def __init__(self, token, platform):
+        self.token = token
+        self.platform = platform
+
+
+    def __str__(self):
+        return self.token
+
+
+class TokenFinder:
+    def __init__(self):
+        tokens = []
+        appdata = f"/Users/{os.getlogin()}/Library/Application Support/"
+
+        paths = {
+        'Discord': appdata + 'discord',
+        }
+
+        for profile in self.get_google_profiles(appdata):
+            paths["Google Chrome"] = profile
+
+        for platform, path in paths.items():
+            if not os.path.exists(path):
+                continue
+            for token in self.extract_tokens(path):
+                tokens.append(Token(token, platform))
+
+        self.tokens = tokens
+
+
+    @staticmethod
+    def extract_tokens(path):
+        path += "/Local Storage/leveldb"
+        tokens = []
+
+        for file_name in os.listdir(path):
+            if not file_name.endswith('.log') and not file_name.endswith('.ldb'):
+                continue
+            for line in [x.strip() for x in open(f'{path}/{file_name}', errors='ignore').readlines() if x.strip()]:
+                for regex in (r'[\w-]{24}\.[\w-]{6}\.[\w-]{27}', r'mfa\.[\w-]{84}'):
+                    for token in re.findall(regex, line):
+                        tokens.append(token)
+        return tokens
+
+
+    @staticmethod
+    def get_google_profiles(appdata):
+        path = appdata + "Google/Chrome/"
+        profiles = []
+
+        for file in os.listdir(path):
+            if file.startswith("Profile"):
+                profiles.append(path+file)
+
+        return profiles
+
+
+    def to_list(self):
+        return [token.token for token in self.tokens]
+
+
+    def display(self):
+        message = ""
+        if len(tokens.to_list()) == 0:
+            message += "No tokens found"
+        else:
+            message += f"Found {len(tokens.to_list())} tokens:"
+            for token in self.tokens:
+                message += "\n"
+                message += f"{token.platform}: {token}"
+
+        return message
 
 
 if __name__ == "__main__":
-    tokens = findTokens()
-    if len(tokens) == 0:
-        print("No tokens found")
-    else:
-        print(f"Found {len(tokens)} tokens:")
-        for token in tokens:
-            print(token)
+    tokens = TokenFinder()
+    print(tokens.display())
